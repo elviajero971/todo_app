15f644fdbb1fa60ba37b1807e1a991380c8caaa4
lucasilliano <lucas.illiano@hotmail.com>
2024-10-07 15:07:13 +0200

    add cors to frontend request to backend

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,9 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: process.env.REACT_APP_FRONTEND_URL,
+  }));
 app.use(express.json());
 
 // Connect to MongoDB
